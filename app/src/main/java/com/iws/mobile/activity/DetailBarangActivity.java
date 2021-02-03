94a543f3740bf0769058d238a07cca353a7fe6be
IWS <indo.wijaya.adm@gmail.com>
2021-02-03 10:46:19 +0700

    detail barang activity

--- app/src/main/java/com/iws/mobile/activity/DetailBarangActivity.java
+++ app/src/main/java/com/iws/mobile/activity/DetailBarangActivity.java
@@ -5,12 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.iws.mobile.R;
+import com.iws.mobile.adapter.DetailBarangSliderAdapter;
+import com.iws.mobile.model.SliderItem;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
 
 public class DetailBarangActivity extends AppCompatActivity {
+    SliderView slider;
+    DetailBarangSliderAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_barang);
+
+        slider = findViewById(R.id.sv_detail_barang);
+        adapter = new DetailBarangSliderAdapter(this);
+
+        adapter.addItem(new SliderItem(R.drawable.slider_test_1));
+        adapter.addItem(new SliderItem(R.drawable.slider_test_2));
+        adapter.addItem(new SliderItem(R.drawable.slider_test_3));
+        slider.setSliderAdapter(adapter);
+        slider.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
+        slider.setScrollTimeInSec(4);
+        slider.setAutoCycle(true);
+        slider.startAutoCycle();
     }
 }
--- app/src/main/java/com/iws/mobile/adapter/ShopAdapter.java
+++ app/src/main/java/com/iws/mobile/adapter/ShopAdapter.java
@@ -1,6 +1,7 @@
 package com.iws.mobile.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.iws.mobile.R;
+import com.iws.mobile.activity.DetailBarangActivity;
 import com.iws.mobile.model.ShopItem;
 
 import java.util.ArrayList;
@@ -63,6 +65,8 @@ public class ShopAdapter extends RecyclerView.Adapter<ShopAdapter.ViewHolder> {
             @Override
             public void onClick(View v) {
                 //do nothing
+                Intent intent = new Intent(context, DetailBarangActivity.class);
+                context.startActivity(intent);
             }
         });
     }
