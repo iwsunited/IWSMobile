b14d684bd8efd4580a0c92be9c54b0d44f45475d
IWS <indo.wijaya.adm@gmail.com>
2021-02-09 14:39:50 +0700

    pembayaran

---
+++ app/src/main/java/com/iws/mobile/activity/PembayaranActivity.java
@@ -0,0 +1,15 @@
+package com.iws.mobile.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.iws.mobile.R;
+
+public class PembayaranActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pembayaran);
+    }
+}
